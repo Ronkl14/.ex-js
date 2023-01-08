060a5ea4f5b1c3c0c6a2112ea836523fa9844ce9
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-08 14:43:46 +0200

    added 2.4

---
+++ 2.4/2.4_function.js
@@ -0,0 +1,11 @@
+function today() {
+  let date = new Date();
+  let day = date.toLocaleString("en-us", { weekday: "long" });
+  let month = date.toLocaleString("en-us", { month: "long" });
+  let dateNum = date.getDate();
+  let year = date.getFullYear();
+
+  return `Today is ${day} the ${dateNum} of ${month} ${year}`;
+}
+
+console.log(today());
