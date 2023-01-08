3a58ac98fa5cefb51a10f9c3a527db1d6d9dcb53
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-08 11:32:31 +0200

    first commit

---
+++ 1.1_primitive.js
@@ -0,0 +1,29 @@
+//string
+let str = "hi";
+console.log(typeof str);
+
+//number
+let num = 4;
+console.log(typeof num);
+
+//boolean
+let bool = false;
+console.log(typeof bool);
+
+//null
+let nu = null;
+console.log(typeof nu);
+
+//undefined
+let unde;
+console.log(typeof unde);
+
+//object
+let obj = { type: "car", brand: "toyota", model: "yaris" };
+console.log(typeof obj);
+
+//symbol
+let sym = Symbol('yo');
+console.log(typeof sym);
+
+
