73fc8e3d2e3454e82b90b146bbc4e440a8bcf2e4
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-08 14:12:01 +0200

    added 2.1

---
+++ 2.1/2.1_function.js
@@ -0,0 +1,10 @@
+function countryInfo (country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+  }
+  
+  let coun1 = countryInfo('Germany', 78, 'Berlin');
+  console.log(coun1)
+  let coun2 = countryInfo('New york', 99, 'Never gonna give you up');
+  console.log(coun2);
+  let coun3 = countryInfo('Israel', 6, 'Not Tel Aviv');
+  console.log(coun3);
