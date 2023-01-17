e65ba4280c05cb2ac46295fc2282665e244a7b60
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-01-17 11:00:26 +0200

    added 13.1

---
+++ 13.1/13.1.js
@@ -0,0 +1,7 @@
+const numbers = [1, -5, 666, 2, 400, 11];
+
+const sortDes = numbers.sort((a, b) => b - a);
+console.log(sortDes);
+
+const sortAsc = numbers.sort((a,b) => a-b);
+console.log(sortAsc);
